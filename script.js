d53fce2b7ad79ff1db37a7bceb65ad28957c03bb
Romain-CRETE <roms.cr@hotmail.fr>
2025-08-14 11:41:03 +0200

    #2 Récupérer le nom des parkings depuis l'API pas tous seul

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const API_ORL =
+  "https://data.orleans-metropole.fr/api/explore/v2.1/catalog/datasets/om-mobilite-parcs-stationnement/records?limit=50";
+
 function getResponse(reponse) {
   console.log("Réponse reçue : " + reponse.status);
   reponse.json().then(getJson);
@@ -6,16 +9,32 @@ function getResponse(reponse) {
 function getJson(json) {
   console.log("JSON", json);
 
-  logNoms(json.results);
+  //logNoms(json.results);
+  updateTable(json.results);
 }
 
-function logNoms(data) {
-  console.log("DATA", data);
-  for (const element of data) {
-    console.log(element.nom);
+function logNoms(parkingList) {
+  console.log("logNoms", parkingList);
+  for (const parking of parkingList) {
+    console.log(parking.nom);
   }
 }
 
-fetch(
-  "https://data.orleans-metropole.fr/api/explore/v2.1/catalog/datasets/om-mobilite-parcs-stationnement/records?limit=50"
-).then(getResponse);
+function updateTable(parkingList) {
+  console.log("updateTable", parkingList);
+
+  let html = "";
+
+  for (const parking of parkingList) {
+    html += "<tr><td>" + parking.nom + "</td><td>Ouvert</td></tr>";
+  }
+
+  // for (let i = 1; i <= parkingList.length; i++) {
+  //   const parking = parkingList[i - 1];
+  //   html += "<tr><td>" + parking.nom + "</td><td>Ouvert</td></tr>";
+  // }
+
+  document.getElementById("parkingList").innerHTML = html;
+}
+
+fetch(API_ORL).then(getResponse);
